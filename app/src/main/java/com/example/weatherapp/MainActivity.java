ac59a16a6efd658a2e62a36ed38ff1552217542d
Pavlo <pavloshtogryn@ukr.net>
2020-07-05 14:30:04 +0300

    added LE check

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnDiscovery;
     Button btnCancelDiscovery;
     Button btnReconnect;
-    //static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private AcceptThread mAcceptThread;
     private ConnectedThread mConnectedThread;
     private CheckBox mCheckBox;
@@ -217,6 +217,13 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this, "Permission has already been granted", Toast.LENGTH_LONG).show();
         }
 
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q){
             LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
             boolean gps_enabled = false;
@@ -246,10 +253,7 @@ public class MainActivity extends AppCompatActivity {
             //Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             //startActivity(intent);
         }
-        //String mydeviceaddress= bluetoothAdapter.getAddress();
-        //String mydevicename= bluetoothAdapter.getName();
-        //status= mydevicename+" : "+ mydeviceaddress;
-        //Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+
 
 
         if (bluetoothAdapter.isDiscovering()) {
